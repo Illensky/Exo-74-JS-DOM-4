7bcaf198630dbc5000f2a448f882cde219370956
Illensky <alexis.laroche.02240@gmail.com>
2021-11-18 10:44:41 +0100

    Exercice done

--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,7 +1,7 @@
 let libButton = document.getElementById('lib-button');
 let libIt = function() {
     let storyDiv = document.getElementById("story");
-    storyDiv.innerHTML = "??Your story here";
+    storyDiv.innerHTML = document.getElementById('noun').value + " " + document.getElementById('adjective').value + " " + document.getElementById('person').value;
 };
 libButton.addEventListener('click', libIt);
 
